d35c5e0c38cac91f51420de131dfe95dddee6f41
TaoL33T <alucardsama04off@gmail.com>
2023-07-29 16:04:06 +0300

    Rewrite

--- main.py
+++ main.py
@@ -2,37 +2,42 @@ import requests
 import argparse
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Obține date meteo despre un anume oraș.")
     parser.add_argument("--oras", required=True, help="Numele orașului.")
     parser.add_argument("--key", required=True, help="API Key")
     return parser.parse_args()
 
+def get_weather_data(oras, api_key):
+    api_url = f"https://api.openweathermap.org/data/2.5/weather?q={oras}&appid={api_key}&units=metric"
+    response = requests.get(api_url)
+    response.raise_for_status()
+    return response.json()
+
+def print_weather_info(data):
+    temp = data["main"]["temp"]
+    wind_speed = data["wind"]["speed"]
+    latitude = data["coord"]["lat"]
+    longitude = data["coord"]["lon"]
+    description = data["weather"][0]["description"]
+    country = data["sys"]["country"]
+
+    print(f"Temperatură: {int(temp)} grade celsius")
+    print(f"Viteza vântului: {wind_speed}")
+    print(f"Latitudine: {latitude}")
+    print(f"Longitudine: {longitude}")
+    print(f"Descriere: {description}")
+    print(f"Țara de origine: {country}")
 
 def main():
     args = parse_args()
-    api = f"https://api.openweathermap.org/data/2.5/weather?q={args.oras}&appid={args.key}&units=metric"
-    info = requests.get(api)
-    date = info.json()
 
     try:
-        temp = date["main"]["temp"]
-        vitezaVantului = date["wind"]["speed"]
-        latitudine = date["coord"]["lat"]
-        longitudine = date["coord"]["lon"]
-        descriere = date["weather"][0]["description"]
-        country = date["sys"]["country"]
-
-        print(f"Temperatură: {int(temp)} grade celsius")
-        print(f"Viteza vântului: {vitezaVantului}")
-        print(f"Latitudine: {latitudine}")
-        print(f"Longitudine: {longitudine}")
-        print(f"Descriere: {descriere}")
-        print(f"Țara de origine: {country}")
-    
-    except:
-        print("Oraș scris incorect sau inexistent")
-    
+        weather_data = get_weather_data(args.oras, args.key)
+        print_weather_info(weather_data)
+    except requests.exceptions.RequestException as e:
+        print(f"A apărut o eroare: {e}")
+    except KeyError:
+        print("Numele orașului este incorect")
 
 if __name__ == '__main__':
     main()
-
